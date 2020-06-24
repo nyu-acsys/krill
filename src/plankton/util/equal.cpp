77937fcd0694669e9339c471d9a9508eed64e7e9
Sebastian Wolff <github@wolff.onl>
2020-06-24 19:07:07 +0200

    syntactic equality (expressions) via syntax tree

--- src/plankton/util/equal.cpp
+++ src/plankton/util/equal.cpp
@@ -7,15 +7,110 @@ using namespace cola;
 using namespace plankton;
 
 
+struct SyntacticEqualityChecker : public BaseVisitor {
+	const Expression* compare_to;
+	bool result;
+	
+	bool is_equal(const Expression& expression, const Expression& other) {
+		compare_to = &other;
+		result = false;
+		expression.accept(*this);
+		return result;
+	}
+
+	template<typename T, typename F = std::function<bool(const T&, const T&)>>
+	inline void handle(const T& expression, F chk_equal) {
+		if (auto other = dynamic_cast<const T*>(compare_to)) {
+			result = chk_equal(expression, *other);
+		}
+	}
+
+	void visit(const VariableDeclaration& node) override {
+		return handle(node, [](const auto& node, const auto& other){
+			return &node == &other;
+		});
+	}
+
+	void visit(const BooleanValue& node) override {
+		return handle(node, [](const auto& node, const auto& other){
+			return node.value == other.value;
+		});
+	}
+
+	void visit(const NullValue& node) override {
+		return handle(node, [](const auto& /*node*/, const auto& /*other*/){
+			return true;
+		});
+	}
+
+	void visit(const EmptyValue& node) override {
+		return handle(node, [](const auto& /*node*/, const auto& /*other*/){
+			return true;
+		});
+	}
+
+	void visit(const MaxValue& node) override {
+		return handle(node, [](const auto& /*node*/, const auto& /*other*/){
+			return true;
+		});
+	}
+
+	void visit(const MinValue& node) override {
+		return handle(node, [](const auto& /*node*/, const auto& /*other*/){
+			return true;
+		});
+	}
+
+	void visit(const NDetValue& node) override {
+		return handle(node, [](const auto& /*node*/, const auto& /*other*/){
+			return true;
+		});
+	}
+
+	void visit(const VariableExpression& node) override {
+		return handle(node, [](const auto& node, const auto& other){
+			return &node.decl == &other.decl;
+		});
+	}
+
+	void visit(const NegatedExpression& node) override {
+		return handle(node, [this](const auto& node, const auto& other){
+			return is_equal(*node.expr, *other.expr);
+		});
+	}
+
+	void visit(const BinaryExpression& node) override {
+		return handle(node, [this](const auto& node, const auto& other){
+			if (node.op != other.op) return false;
+			if (is_equal(*node.lhs, *other.lhs) && is_equal(*node.rhs, *other.rhs)) return true;
+			if (is_equal(*node.lhs, *other.rhs) && is_equal(*node.rhs, *other.lhs)) return true;
+			return false;
+		});
+	}
+
+	void visit(const Dereference& node) override {
+		return handle(node, [this](const auto& node, const auto& other){
+			return node.fieldname == other.fieldname && is_equal(*node.expr, *other.expr);
+		});
+	}
+};
+
 bool plankton::syntactically_equal(const Expression& expression, const Expression& other) {
-	// TODO: find a better way to do this
-	// NOTE: this relies on two distinct VariableDeclaration objects not having the same name
-	std::stringstream expressionStream, otherStream;
-	cola::print(expression, expressionStream);
-	cola::print(other, otherStream);
-	return expressionStream.str() == otherStream.str();
+	static SyntacticEqualityChecker visitor;
+	return visitor.is_equal(expression, other);
+
+	// // // TODO: use the following code for testing?
+	// // NOTE: this relies on two distinct VariableDeclaration objects not having the same name
+	// std::stringstream expressionStream, otherStream;
+	// cola::print(expression, expressionStream);
+	// cola::print(other, otherStream);
+	// return expressionStream.str() == otherStream.str();
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct LogicSyntacticEqualityChecker : public LogicVisitor {
 	const Formula* compare_to;
@@ -46,7 +141,7 @@ struct LogicSyntacticEqualityChecker : public LogicVisitor {
 	}
 
 	template<typename T, typename F = std::function<bool(const T&, const T&)>>
-	inline void handle(const T& formula, F chk_equal) {
+	inline void handle(const T& formula, const F& chk_equal) {
 		if (auto other = dynamic_cast<const T*>(compare_to)) {
 			result = chk_equal(formula, *other);
 		}
