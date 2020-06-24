53b47293c4831fe6c7ae9f773bb9b536f794da0e
Sebastian Wolff <github@wolff.onl>
2020-06-24 16:08:20 +0200

    annotation equality

--- src/plankton/util/solving.cpp
+++ src/plankton/util/solving.cpp
@@ -6,18 +6,30 @@ using namespace cola;
 using namespace plankton;
 
 
+bool check_implication(const Formula& /*premise*/, const Formula& /*otherPremise*/, const Formula& /*conclusion*/) {
+	throw std::logic_error("not yet implemented (check_implication)");
+}
+
+bool check_implication(const Formula& premise, const Formula& conclusion) {
+	static ExpressionAxiom trueFormula(std::make_unique<BooleanValue>(true));
+	return check_implication(premise, trueFormula, conclusion);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 bool plankton::implies_nonnull(const Formula& /*formula*/, const cola::Expression& /*expr*/) {
-	throw std::logic_error("not yet implemented (plankton::implies_nonnull)");
 	// TODO: find more efficient implementation that avoids copying expr
-	// BinaryExpression nonnull(BinaryExpression::Operator::NEQ, cola::copy(expr), std::make_unique<NullValue>());
-	// return plankton::implies(formula, nonnull);
+	BinaryExpression nonnull(BinaryExpression::Operator::NEQ, cola::copy(expr), std::make_unique<NullValue>());
+	return plankton::implies(formula, nonnull);
 }
 
 bool plankton::implies(const Formula& /*formula*/, const Expression& /*implied*/) {
-	throw std::logic_error("not yet implemented (plankton::implies)");
 	// TODO: find more efficient implementation that avoids copying expr
-	// ExpressionAxiom axiom(cola::copy(implied));
-	// return plankton::implies(formula, axiom);
+	ExpressionAxiom axiom(cola::copy(implied));
+	return plankton::implies(formula, axiom);
 }
 
 bool plankton::implies(const Formula& /*formula*/, const Formula& /*implied*/) {
@@ -26,23 +38,6 @@ bool plankton::implies(const Formula& /*formula*/, const Formula& /*implied*/) {
 
 bool plankton::implies(const Formula& /*formula*/, const std::vector<std::reference_wrapper<const Formula>>& /*implied*/) {
 	throw std::logic_error("not yet implemented (plankton::implies)");
-}
-
-bool plankton::is_equal(const Annotation& /*annotation*/, const Annotation& /*other*/) {
-	throw std::logic_error("not yet implemented (plankton::is_equal)");
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-bool check_implication(const Formula& /*premise*/, const Formula& /*otherPremise*/, const Formula& /*conclusion*/) {
-	throw std::logic_error("not yet implemented (check_implication)");
-}
-
-bool check_implication(const Formula& premise, const Formula& conclusion) {
-	static ExpressionAxiom trueFormula(std::make_unique<BooleanValue>(true));
-	return check_implication(premise, trueFormula, conclusion);
 }
 
 
@@ -115,14 +110,6 @@ struct TimeIterator : public PieceIterator<TimePredicate> {
 	TimeIterator(const std::vector<std::unique_ptr<Annotation>>& container) : PieceIterator(container) {}
 };
 
-bool annotation_implies(const Annotation& annotation, const Axiom& formula) {
-	return check_implication(*annotation.now, formula);
-}
-
-bool annotation_implies(const Annotation& annotation, const ImplicationFormula& formula) {
-	return check_implication(*annotation.now, *formula.premise, *formula.conclusion);
-}
-
 bool time_implies(const PastPredicate& premise, const PastPredicate& conclusion) {
 	return check_implication(*premise.formula, *conclusion.formula);
 }
@@ -132,7 +119,7 @@ bool time_implies(const FuturePredicate& premise, const FuturePredicate& conclus
 }
 
 template<typename T, typename = std::enable_if_t<std::is_base_of_v<TimePredicate, T>>>
-bool annotation_implies(const Annotation& annotation, const T& conclusion) {
+bool annotation_implies_time(const Annotation& annotation, const T& conclusion) {
 	for (const auto& premise : annotation.time) {
 		auto check = plankton::is_of_type<T>(*premise);
 		if (check.first && time_implies(*check.second, conclusion)) {
@@ -144,26 +131,31 @@ bool annotation_implies(const Annotation& annotation, const T& conclusion) {
 
 bool annotation_implies(const Annotation& annotation, const Formula& formula) {
 	if (const Axiom* axiom = dynamic_cast<const Axiom*>(&formula)) {
-		return annotation_implies(annotation, *axiom);
+		return check_implication(*annotation.now, formula);
 
 	} else if (const ImplicationFormula* imp = dynamic_cast<const ImplicationFormula*>(&formula)) {
-		return annotation_implies(annotation, *imp);
+		return check_implication(*annotation.now, *imp->premise, *imp->conclusion);
 
 	} else if (const PastPredicate* pred = dynamic_cast<const PastPredicate*>(&formula)) {
-		return annotation_implies<PastPredicate>(annotation, *pred);
+		return annotation_implies_time(annotation, *pred);
 
 	} else if (const FuturePredicate* pred = dynamic_cast<const FuturePredicate*>(&formula)) {
-		return annotation_implies<FuturePredicate>(annotation, *pred);
+		return annotation_implies_time(annotation, *pred);
+
+	} else if (const ConjunctionFormula* con = dynamic_cast<const ConjunctionFormula*>(&formula)) {
+		return check_implication(*annotation.now, *con);
 	}
 
 	std::string name(typeid(formula).name());
 	throw std::logic_error("Case distinction was supposed to be complete, unexpected type with typeid '" + name + "'.");
 }
 
-bool all_imply(const std::vector<std::unique_ptr<Annotation>>& annotations, const Formula& conclusion, const Annotation* skip=nullptr) {
-	for (const auto& annotation : annotations) {
-		if (annotation.get() == skip) continue;
-		if (!annotation_implies(*annotation, conclusion)) {
+template<typename T>
+bool all_imply(const T& container, const Formula& conclusion, const Annotation* skip=nullptr) {
+	for (const auto& elem : container) {
+		const Annotation& annotation = *elem;
+		if (&annotation == skip) continue;
+		if (!annotation_implies(annotation, conclusion)) {
 			return false;
 		}
 	}
@@ -190,4 +182,20 @@ std::unique_ptr<Annotation> plankton::unify(std::vector<std::unique_ptr<Annotati
 	}
 
 	return result;
+}
+
+bool annotation_implies(const Annotation& premise, const Annotation& conclusion) {
+	if (!check_implication(*premise.now, *conclusion.now)) return false;
+	if (!check_implication(*conclusion.now, *premise.now)) return false;
+	for (const auto& pred : conclusion.time) {
+		if (!annotation_implies(premise, *pred)) return false;
+	}
+	for (const auto& pred : premise.time) {
+		if (!annotation_implies(conclusion, *pred)) return false;
+	}
+	return true;
+}
+
+bool plankton::is_equal(const Annotation& annotation, const Annotation& other) {
+	return annotation_implies(annotation, other) && annotation_implies(other, annotation);
 }
